1c10a11964c2506912a0dc5736b1370bf296514b
FredsephT32 <106403769+FredsephT32@users.noreply.github.com>
2022-09-05 18:22:20 -0400

    Last minor changes done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function toggleNav() {
-  const hamburgerElement = document.getElementById('Hamburger-logo');
+  const hamburgerElement = document.getElementById('Hamburger-icon');
   const closeElement = document.getElementsByClassName('close-btn')[0];
   const mobileLinksElement = document.getElementsByClassName('mob-menu')[0];
   hamburgerElement.classList.toggle('isHidden');
@@ -8,6 +8,6 @@ function toggleNav() {
   mobileLinksElement.classList.toggle('isHidden');
 }
 
-document.getElementById('Hamburger-logo').addEventListener('click', toggleNav);
+document.getElementById('Hamburger-icon').addEventListener('click', toggleNav);
 document.getElementsByClassName('close-btn')[0].addEventListener('click', toggleNav);
 document.getElementsByClassName('mob-menu')[0].addEventListener('click', toggleNav);
